5b3d5576f96fb1869b7f941a5165cb71fb6121e4
lcm-homebrew-internal <lcmsoft@uwl.ac.uk>
2022-08-26 17:00:13 +0100

    Update audient-id.rb

--- Casks/audient-id.rb
+++ Casks/audient-id.rb
@@ -5,7 +5,7 @@ cask "audient-id" do
   url "http://10.0.39.53:8089/apps/audient/iD%20v4.3.4.dmg"
   name "Audient iD"
   desc "Audient audio interface control GUI"
-  homepage "https://avid.com"
+  homepage "https://audient.com"
   
   app "iD.app"
 end
