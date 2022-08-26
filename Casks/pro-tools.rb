315ffb6b6e0621d30c5123af3c726e69678df204
lcm-homebrew-internal <lcmsoft@uwl.ac.uk>
2022-08-26 16:02:59 +0100

    Create pro-tools.rb

---
+++ Casks/pro-tools.rb
@@ -0,0 +1,11 @@
+cask "pro-tools" do
+  version "2022.7"
+  sha :no_check
+  
+  url "http://10.0.39.53:8089/apps/avid/Pro_Tools_2022.7_Mac.dmg"
+  name "Pro Tools"
+  desc "Digital Audio Workstation"
+  homepage "https://avid.com"
+  
+  pkg "Install Pro Tools 2022.7.0.pkg"
+end
