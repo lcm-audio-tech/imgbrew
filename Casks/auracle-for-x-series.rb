f8d152ef1886d24b017dc23d9b8fec94bdfec5dc
lcm-homebrew-internal <lcmsoft@uwl.ac.uk>
2022-08-26 16:52:57 +0100

    Create auracle-for-x-series.rb

---
+++ Casks/auracle-for-x-series.rb
@@ -0,0 +1,11 @@
+cask "auracle-for-x-series" do
+  version "1.9.0"
+  sha256 :no_check
+  
+  url "http://10.0.39.53:8089/apps/iconnectivity/AuracleForX1.9.0.dmg"
+  name "Auracle for X Series"
+  desc "Digital Audio Workstation"
+  homepage "https://avid.com"
+  
+  app "Auracle for X-Series.app"
+end
