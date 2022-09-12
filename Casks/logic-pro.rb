b6fdc33dce074299c1c3aacd28abe09a6619ee69
lcm-homebrew-internal <lcmsoft@uwl.ac.uk>
2022-09-12 16:11:20 +0100

    Create logic-pro.rb

---
+++ Casks/logic-pro.rb
@@ -0,0 +1,11 @@
+cask "logic-pro" do
+  version "10.7.4"
+  sha256 :no_check
+  
+  url "http://10.0.39.53:8089/apps/apple/LogicPro_10.7.4.dmg"
+  name "Logic Pro"
+  desc "Digital Audio Workstation"
+  homepage "https://apple.com"
+  
+  pkg "LogicPro_10.7.4.pkg"
+end
