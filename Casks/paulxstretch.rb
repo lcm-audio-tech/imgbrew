e0a678ebcf35b844d4488226602d7a91b88d5232
lcm-homebrew-internal <lcmsoft@uwl.ac.uk>
2022-09-13 10:44:40 +0100

    add DaVinci Resolve

---
+++ Casks/paulxstretch.rb
@@ -0,0 +1,11 @@
+cask "paulxstretch" do
+  version "1.6.0"
+  sha256 "c5bcba5215076f629c3442902eaba4825a7f71ac33fc2d793c7cb729bdeb17ad"
+
+  url "http://10.0.39.53:8089/apps/sonosaurus/paulxstretch-1.6.0-mac.dmg"
+  name "PaulXStretch"
+  desc "Audio stretching tool."
+  homepage "https://sonosaurus.com"
+  
+  pkg "PaulXStretch Installer.pkg"
+end
