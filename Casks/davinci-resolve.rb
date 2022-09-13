a976b9453c2ecbd31e1cdc7eb878f36fa77bf44b
lcm-homebrew-internal <lcmsoft@uwl.ac.uk>
2022-09-13 10:47:23 +0100

    Update davinci-resolve.rb

--- Casks/davinci-resolve.rb
+++ Casks/davinci-resolve.rb
@@ -2,7 +2,7 @@ cask "davinci-resolve" do
   version "18.0.2"
   sha256 "96925e7ced038bda8d85028c0d1679fdadbfff56803b0be2dccae634c624f530"
   
-  url "http://10.0.39.53:8089/apps/blackmagic/DaVinci_Resolve_18.0.2_Mac"
+  url "http://10.0.39.53:8089/apps/blackmagic/DaVinci_Resolve_18.0.2_Mac.dmg"
   name "DaVinci Resolve 18"
   desc "Non-linear editor."
   homepage "https://blackmagicdesign.com"
