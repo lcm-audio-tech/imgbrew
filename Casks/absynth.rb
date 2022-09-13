93a02d277c2e8017fafb4fb09f50633a4e923a9a
lcm-homebrew-internal <lcmsoft@uwl.ac.uk>
2022-09-13 15:18:46 +0100

    add b4 and a5

---
+++ Casks/absynth.rb
@@ -0,0 +1,10 @@
+cask "absynth" do
+  version "5"
+  sha256 "eab6ce2ecd6efd4bf276de0b024b6f9cca1617d342085bec96233335f6eb60b4"
+  
+  url "http://10.0.39.53:8089/apps/native-instruments/Absynth_5_Installer.dmg"
+  name "Absynth"
+  desc "Soon-to-be-retired synth."
+  
+  pkg "Absynth 5 Installer Mac.pkg"
+end
---
+++ Casks/battery.rb
@@ -0,0 +1,10 @@
+cask "battery" do
+  version "4"
+  sha256 "eab6ce2ecd6efd4bf276de0b024b6f9cca1617d342085bec96233335f6eb60b4"
+  
+  url "http://10.0.39.53:8089/apps/native-instruments/Battery_4_Installer.dmg"
+  name "Battery"
+  desc "Doof machine."
+  
+  pkg "Battery 4 Installer Mac.pkg"
+end
