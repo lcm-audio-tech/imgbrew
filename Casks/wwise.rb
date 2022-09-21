165b276dbf3cebe8ba1c6ced3118c6135a0648fe
lcm-homebrew-internal <lcmsoft@uwl.ac.uk>
2022-09-21 15:15:28 +0100

    Update wwise.rb

--- Casks/wwise.rb
+++ Casks/wwise.rb
@@ -1,6 +1,6 @@
 cask "wwise" do
   version "2021.1.10.7883"
-  sha256 "c8b385fa028b822e50dfb4922f2d5f9d7955fec709c0629655ecf0bf808994e4"
+  sha256 "1252ba153fd1d5c0c2cc2640ce8bceb253af347d4a36acb34c3b20940f186783"
   
   url "http://10.0.39.53:8089/apps/audiokinetic/WwiseEditor.dmg"
   name "Wwise editor"
