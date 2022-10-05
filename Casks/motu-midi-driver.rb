d0ce9776319f4ea419e2a397494e7f89fbe0a5e8
lcm-homebrew-internal <lcmsoft@uwl.ac.uk>
2022-10-05 16:37:30 +0100

    Update motu-midi-driver.rb

--- Casks/motu-midi-driver.rb
+++ Casks/motu-midi-driver.rb
@@ -2,10 +2,10 @@ cask "motu-midi-driver" do
   version "91217"
   sha256 :no_check
   
-  url "https://cdn-data.motu.com/downloads/midi/driver/RC/MOTU MIDI Installer (Dext 91217).pkg"
+  url "https://cdn-data.motu.com/downloads/midi/driver/RC/MOTU%20MIDI%20Installer%20(Dext%2091217).pkg"
   name "MOTU MIDI Driver"
   desc "Digital Audio Workstation"
   homepage "https://www.motu.com"
   
-  pkg "MOTU MIDI Installer (Dext 91217).pkg"
+  pkg "MOTU%20MIDI%20Installer%20(Dext%2091217).pkg"
 end
