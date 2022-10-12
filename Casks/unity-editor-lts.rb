7644ac97829e233613ec0597519279a8a71050c6
lcm-homebrew-internal <lcmsoft@uwl.ac.uk>
2022-10-12 11:48:58 +0100

    Update unity-editor-lts.rb

--- Casks/unity-editor-lts.rb
+++ Casks/unity-editor-lts.rb
@@ -2,7 +2,7 @@ cask "unity-editor-lts" do
   version "2021-3-11f1-LTS"
   sha256 "99313192f33805f9c8d8f9c004233aac62f1b884e3712e700e1e53a9e9e64b90"
   
-  url "http://10.0.39.53:8089/apps/unity/2021-3-10f1-LTS/Unity-Editor-2021-3-11f1-LTS.dmg"
+  url "http://10.0.39.53:8089/apps/unity/2021-3-11f1-LTS/Unity-Editor-2021-3-11f1-LTS.dmg"
   name "Unity editor — long term support version"
   desc "Editor for Unity"
   homepage "https://unity.com"
