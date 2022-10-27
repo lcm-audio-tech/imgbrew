b2cab697180dcec0ec37577dfea88d0bd5720c50
lcm-homebrew-internal <lcmsoft@uwl.ac.uk>
2022-10-27 11:43:50 +0100

    Create midi-control-center.rb

---
+++ Casks/midi-control-center.rb
@@ -0,0 +1,11 @@
+cask "midi-control-center" do
+  version "1.14.0.303"
+  sha256 "c76f42f8da3aa56fa4a880922f247721555eaddb5cd4d86fab002a538f707a85"
+  
+  url "http://10.0.39.53:8089/apps/arturia/MIDI_Control_Center_1_14_0_303.pkg"
+  name "MIDI Control Center"
+  desc "Utility for Arturia's hardware instruments"
+  homepage "https://www.arturia.com"
+  
+  pkg "MIDI_Control_Center_1_14_0_303.pkg"
+end
