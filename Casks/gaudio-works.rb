baf8ec369fbef1c51150e6c15cec9cf57af578e0
lcm-homebrew-internal <lcmsoft@uwl.ac.uk>
2023-03-03 13:39:37 +0000

    Create gaudio-works.rb

---
+++ Casks/gaudio-works.rb
@@ -0,0 +1,10 @@
+cask "gaudio-works" do
+  version "2.1.3.a"
+  sha256 "80f3470bf43487e51e5d8551dea92af608a1182d0531b90f579fe1f0fe1d5bae"
+  
+  url "http://10.0.39.53:8089/apps/gaudio/Gaudio_Works_2.1.3_a.pkg"
+  name "Works"
+  desc "Gaudio Works spatial audio authoring suite."
+  
+  pkg "Gaudio_Works_2.1.3_a.pkg"
+end
