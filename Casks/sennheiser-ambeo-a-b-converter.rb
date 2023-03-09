4fc330f780fce4b66bd7773595754417e817a1c1
lcm-homebrew-internal <lcmsoft@uwl.ac.uk>
2023-03-09 15:35:19 +0000

    Create sennheiser-ambeo-a-b-converter.rb

---
+++ Casks/sennheiser-ambeo-a-b-converter.rb
@@ -0,0 +1,10 @@
+cask "sennheiser-ambeo-a-b-converter" do
+  version "1.2.1"
+  sha256 "6c1bc354a41737d3aff2d083e1e0fcad157e364810a29dcd54d2a5216fd8fe7b"
+  
+  url "https://assets.sennheiser.com/global-downloads/file/11091/Sennheiser_AMBEO_A-B_Converter_1.2.1_MAC.zip"
+  name "Ambeo A-B Format Converter"
+  desc "Converter for Sennheiser Ambeo Ambisonic microphone."
+  
+  pkg "Install Sennheiser AMBEO VR Mic A-B Converter 1.2.0.pkg"
+end
